9b47092d4e750e332988e721972380654695fb27
--replace-all <chughtai.mh@gmail.com>
2016-11-20 11:53:16 -0500

    Added docstring to credentials_parser.py

--- bot.py
+++ bot.py
@@ -4,6 +4,8 @@ DATABASE_CONFIG_FILEPATH = __file__.strip('/bot.py') + r'credentials.cfg'
 
 
 def credentials_parser(file_path=DATABASE_CONFIG_FILEPATH):
+	"""Returns a credentials SafeConfigParser object from :file_path: 
+	location."""
 	parser = SafeConfigParser()
 	parser.read(file_path)
 	return parser
