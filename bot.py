baa9ff09881d5aff04d8c5afe8689a2dc899164a
--replace-all <chughtai.mh@gmail.com>
2016-11-20 12:51:23 -0500

    Initial commit of SlackBot class

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from utils import get_slack_token
 from configparser import SafeConfigParser
 from slackclient import SlackClient
 
@@ -10,3 +11,27 @@ def credentials_parser(file_path=DATABASE_CONFIG_FILEPATH):
 	parser = SafeConfigParser()
 	parser.read(file_path)
 	return parser
+
+
+class SlackBot(SlackClient):
+
+	"""Main SlackBot class that extends slackclient.SlackClient"""
+
+	def __init__(self, token):
+		"""token = Slack API token"""
+		super(SlackBot, self).__init__(token)
+
+	def talk(self, as_user=True, **kwargs):
+		"""Makes a chat post message api call to slack.
+		Args:
+				as_user -> bool	:	Whether or not to include the bot's profile in
+														the message
+				channel -> str 	:	channel to post message to
+				text	-> str 	: 	Message to send
+		"""
+		return self.api_call('chat.postMessage', as_user=as_user, **kwargs)
+
+	def connect(self, is_reconnect=False):
+		"""Connects to Slack API. Set is_reconnect to True in case of 
+		timeout."""
+		return self.rtm_connect(is_reconnect)
