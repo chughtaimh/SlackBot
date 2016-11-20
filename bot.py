f92a010491f497d0b15071c0842928427a5dac9d
--replace-all <chughtai.mh@gmail.com>
2016-11-20 11:52:17 -0500

    Added get_jira_credentials function

--- bot.py
+++ bot.py
@@ -7,3 +7,8 @@ def credentials_parser(file_path=DATABASE_CONFIG_FILEPATH):
 	parser = SafeConfigParser()
 	parser.read(file_path)
 	return parser
+
+
+def get_jira_credentials(parser):
+	"""Returns jira credentials"""
+	return parser.get('jira', 'user'), parser.get('jira', 'pass')
