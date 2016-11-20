5565376936aaf6460023d56571d2a882d81d4fa6
--replace-all <chughtai.mh@gmail.com>
2016-11-20 11:54:33 -0500

    added get_slack_token function

--- bot.py
+++ bot.py
@@ -12,5 +12,10 @@ def credentials_parser(file_path=DATABASE_CONFIG_FILEPATH):
 
 
 def get_jira_credentials(parser):
-	"""Returns jira credentials"""
+	"""Returns jira credentials."""
 	return parser.get('jira', 'user'), parser.get('jira', 'pass')
+
+
+def get_slack_token(parser):
+	"""Returns slack token."""
+	return parser.get('slack', 'token')
